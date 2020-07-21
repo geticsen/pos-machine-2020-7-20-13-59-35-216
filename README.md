7d2c203c4ce121d38c9ad7ea8e431c367185ee41
geticsen <286348794@qq.com>
2020-07-22 00:04:45 +0800

    finished the task

---
+++ README.md
@@ -0,0 +1,58 @@
+# context map
+![context map](./pos-context map.png "optional title")
+
+|  createReceipt()   | Description  |
+|  ----  | ----  |
+| P  | 10 mins |
+| D  | 30 mins |
+| C  | 字符串拼接转义字符的问题 |
+| A  | 换用字符串模板 |
+
+|  printReceipt()   | Description  |
+|  ----  | ----  |
+| P  | 3mins |
+| D  | 2 mins |
+| C  | none |
+| A  | none |
+
+|  filterNotExistBarcodes()   | Description  |
+|  ----  | ----  |
+| P  | 10 mins |
+| D  | 8 mins |
+| C  | none |
+| A  | none |
+
+|  getBarcodeInfo()   | Description  |
+|  ----  | ----  |
+| P  | 5 mins |
+| D  | 4 mins |
+| C  | none |
+| A  | none |
+
+|  calculate()   | Description  |
+|  ----  | ----  |
+| P  | 1 mins |
+| D  | 1 mins |
+| C  | none |
+| A  | none |
+
+|  findAllBarcodesData()   | Description  |
+|  ----  | ----  |
+| P  | 1 mins |
+| D  | 5 mins |
+| C  | 一开始没有找到数据，后来发现数据在read.me上 |
+| A  | 注意看文档 |
+
+|  checkBarcodeExist()   | Description  |
+|  ----  | ----  |
+| P  | 5 mins |
+| D  | 3 mins |
+| C  | none |
+| A  | none |
+
+|  countTimes()   | Description  |
+|  ----  | ----  |
+| P  | 5 mins |
+| D  | 10 mins |
+| C  | 不知道json怎么判断是否包含某个key值 |
+| A  | 查询得到json对象有判断是否包含某个key的函数 |
---
+++ main.js
@@ -0,0 +1,102 @@
+function printReceipt(barcodes) {
+    filterbarcodes = filterNotExistBarcodes(barcodes);
+    var receipt = createReceipt(filterbarcodes);
+    console.log(receipt);
+}
+
+function createReceipt(barcodes){
+    var title = "\n***<store earning no money>Receipt ***\n";
+    var content = title;
+    var total = 0;
+    for(var key in barcodes){
+        barcodeInfo = getBarcodeInfo(key);
+        var num = barcodes[key];
+        var Subtotal = calculate(num,barcodeInfo["price"])
+        total+= Subtotal;
+        
+        content+=`Name: ${barcodeInfo["name"]}, Quantity: ${num}, Unit price: ${barcodeInfo["price"]} (yuan), Subtotal: ${Subtotal} (yuan)\n`;
+        
+    }
+    content+="----------------------\n"+
+    "Total: "+ total +" (yuan)\n"+
+    "**********************";
+    return content;
+}
+function filterNotExistBarcodes(barcodes){
+    barcodes = countTimes(barcodes);
+    var filterBarcodes=[];
+    for(var key in barcodes){
+        if(checkBarcodeExist(key)){
+            filterBarcodes[key]=barcodes[key];
+        }
+    }
+    return filterBarcodes;
+}
+function countTimes(barcodes){
+    var mergeConmmon = [];
+    for(var i=0;i<barcodes.length;i++){
+        if(mergeConmmon.hasOwnProperty(barcodes[i])){
+            mergeConmmon[barcodes[i]] += 1;
+        }else{
+            mergeConmmon[barcodes[i]] = 1;
+        }
+    }
+    return mergeConmmon;
+}
+function checkBarcodeExist(barcode){
+    var isExist =true;
+    if(getBarcodeInfo(barcode)==""){
+        isExist = false;
+    }
+    return isExist;
+}
+function findAllBarcodesData(){
+    return [
+        {
+           'barcode': 'ITEM000000',
+           'name': 'Coca-Cola',
+           'price': 3
+         },
+         {
+           'barcode': 'ITEM000001',
+           'name': 'Sprite',
+           'price': 3
+         },
+         {
+           'barcode': 'ITEM000002',
+           'name': 'Apple',
+           'price': 5
+         },
+         {
+           'barcode': 'ITEM000003',
+           'name': 'Litchi',
+           'price': 15
+         },
+         {
+           'barcode': 'ITEM000004',
+           'name': 'Battery',
+           'price': 2
+         },
+         {
+           'barcode': 'ITEM000005',
+           'name': 'Instant Noodles',
+           'price': 4
+         }
+     ];
+}
+function getBarcodeInfo(barcode){
+    var barcodesInfo = findAllBarcodesData();
+    var barcodeInfo="";
+    for(var i=0;i<barcodesInfo.length;i++){
+        if(barcodesInfo[i]["barcode"] == barcode){
+            barcodeInfo = barcodesInfo[i];
+        }
+    }
+    return barcodeInfo;
+}
+function calculate(num,price){
+    return num*price;
+}
+module.exports = {
+    printReceipt
+};
